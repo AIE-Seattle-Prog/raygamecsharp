01f07e8627a55b778060054d6ddeffe6aa7dd39f
Terry Nguyen <terry@terrehbyte.com>
2020-08-28 10:21:05 -0700

    Add good defaults for using directives

--- raygamecsharp/Program.cs
+++ raygamecsharp/Program.cs
@@ -8,20 +8,24 @@
 *   Note that compiled executable is placed in the same folder as .c file
 *
 *   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
 *
 *   Enjoy using raylib. :)
 *
-*   This example has been created using raylib 1.0 (www.raylib.com)
+*   This example has been created using raylib-cs 3.0 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
+*   This example was lightly modified to provide additional 'using' directives to make
+*   common math types and utilities readily available, though they are not using in this sample.
+*
+*   Copyright (c) 2019-2020 Academy of Interactive Entertainment (@aie_usa)
 *   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
-using Raylib_cs;
-using static Raylib_cs.Raylib;
-using static Raylib_cs.Color;
+using static Raylib_cs.Raylib;  // core methods (InitWindow, BeginDrawing())
+using static Raylib_cs.Color;   // color (RAYWHITE, MAROON, etc.)
+using static Raylib_cs.Raymath; // mathematics utilities and operations (Vector2Add, etc.)
+using System.Numerics;          // mathematics types (Vector2, Vector3, etc.)
 
 namespace Examples
 {
